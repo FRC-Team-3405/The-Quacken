9267b50f94e9eb17f7bdf9900dc91725fe04ecf7
Matthew Whitaker <sub6resources@gmail.com>
2019-01-08 16:10:34 -0700

    Convert main to kotlin

--- src/main/java/frc/robot/Main.kt
+++ src/main/java/frc/robot/Main.kt
@@ -5,25 +5,25 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc.robot
 
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotBase
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all.
  * Unless you know what you are doing, do not modify this file except to
  * change the parameter class to the startRobot call.
  */
-public final class Main {
-  private Main() {
-  }
+object Main {
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     *
+     * If you change your main robot class, change the parameter type.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        RobotBase.startRobot<Robot> { Robot() }
+    }
 }
